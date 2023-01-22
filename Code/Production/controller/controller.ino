bbfa338cb90303b43596707f6b1d837e85218b3a
Alexander Lehmann <a.lehmann@protonmail.com>
2023-01-22 15:59:46 +0100

    update code

--- Code/Production/controller/controller.ino
+++ Code/Production/controller/controller.ino
@@ -2,76 +2,68 @@
 
 int CHANNELS[] = { 15, 16, 14, 8 };
 int INPUTS[] = { 18, 19, 20, 21 };
-int VALUES[14][4];
+int LEDS[] = { 4, 5, 6, 7 };
+int VALUES[16][5];
+int MASK[16][5];
 
 void setup() {
   Wire.begin();
 
+  getMask();
+
   for (int i = 0; i < 4; i++) {
     pinMode(CHANNELS[i], OUTPUT);
+    pinMode(LEDS[i], OUTPUT);
     pinMode(INPUTS[i], INPUT);
   }
 }
 
 void loop() {
+
   Wire.requestFrom(42, 8, false);
 
-  Serial.println("---");
-
-  while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
-  }
-
-  Serial.print("\n");
-
-  /*
-  Serial.println("---");
-  setChannel(13);
-  Serial.println(analogRead(INPUTS[2]));
-  setChannel(0);
-  Serial.println(analogRead(INPUTS[0]));
-  */
-
-  int OLD_VALUES[14][4];
-  for (int i = 0; i < 14; i++) {
-    for (int j = 0; j < 4; j++) {
+  int OLD_VALUES[16][5];
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 5; j++) {
       OLD_VALUES[i][j] = VALUES[i][j];
     }
   }
 
-  for (int i = 0; i < 14; i++) {
+  int incomingCounter = 0;
+  while (Wire.available()) {
+    int value = int(Wire.read()) - 63;
+    VALUES[incomingCounter][4] = value;
+    incomingCounter++;
+  }
+
+  for (int i = 0; i < 16; i++) {
     setChannel(i);
     for (int j = 0; j < 4; j++) {
-      if (j == 2 && i >= 6 && i <= 12) continue;
-      if (j == 3 && i >= 7 && i <= 12) continue;
       VALUES[i][j] = analogRead(INPUTS[j]);
     }
   }
 
-  /*
-  for (int i = 0; i < 14; i++) {
-    for (int j = 0; j < 4; j++) {
-      if (OLD_VALUES[i][j] != VALUES[i][j]) {
-        Serial.println("Change: " + String(i) + ", " + String(j) + ": " + String(VALUES[i][j]));
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (OLD_VALUES[i][j] != VALUES[i][j] && MASK[i][j] > 0) {
+        Serial.println("Change: " + String(j) + ", " + String(i) + ": " + String(VALUES[i][j]));
       }
     }
   }
-*/
-
-  Serial.println(" ");
-  for (int j = 0; j < 4; j++) {
-    Serial.print(String(j) + ": ");
-    for (int i = 0; i < 14; i++) {
-      Serial.print(String(VALUES[i][j]) + "\t");
-    }
-    Serial.print("\n");
-  }
-  Serial.println(" ");
-  Serial.println(" ");
-  delay(500);
 }
 
 void setChannel(int channel) {
   PORTB = channel << 1;
+}
+
+void getMask() {
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 5; j++) {
+      MASK[i][j] = 1;
+      if (j == 2 && i >= 6 && i <= 12) MASK[i][j] = 0;
+      if (j == 3 && i >= 7 && i <= 12) MASK[i][j] = 0;
+      if (j == 4 && i >= 8) MASK[i][j] = 0;
+      if (i >= 14) MASK[i][j] = 0;
+    }
+  }
 }
--- Code/Test/controller_test_02/controller_test_02.ino
+++ Code/Test/controller_test_02/controller_test_02.ino
@@ -2,18 +2,33 @@
 
 int CHANNELS[] = { 15, 16, 14, 8 };
 int INPUTS[] = { 18, 19, 20, 21 };
-int VALUES[14][4];
+int LEDS[] = { 4, 5, 6, 7 };
+int VALUES[16][4];
+int MASK[16][4];
+
+int counter;
 
 void setup() {
   Wire.begin();
 
+  getMask();
+
   for (int i = 0; i < 4; i++) {
     pinMode(CHANNELS[i], OUTPUT);
+    pinMode(LEDS[i], OUTPUT);
     pinMode(INPUTS[i], INPUT);
   }
+
+  counter = 0;
 }
 
 void loop() {
+
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(LEDS[i], counter & 1 << i);
+  }
+  counter++;
+
   Wire.requestFrom(42, 8, false);
   Serial.print("\tR:\t");
 
@@ -24,19 +39,22 @@ void loop() {
 
   Serial.print("\n");
 
-  for (int i = 0; i < 14; i++) {
+  for (int i = 0; i < 16; i++) {
     setChannel(i);
     for (int j = 0; j < 4; j++) {
-      if (j == 2 && i >= 6 && i <= 12) continue;
-      if (j == 3 && i >= 7 && i <= 12) continue;
       VALUES[i][j] = analogRead(INPUTS[j]);
     }
   }
 
   for (int j = 0; j < 4; j++) {
     Serial.print("\t" + String(j) + ":\t");
-    for (int i = 0; i < 14; i++) {
-      Serial.print(String(VALUES[i][j]) + "\t");
+    for (int i = 0; i < 16; i++) {
+      if (MASK[i][j] > 0) {
+        Serial.print(String(VALUES[i][j]));
+      } else {
+        Serial.print("-");
+      }
+      Serial.print("\t");
     }
     Serial.print("\n");
   }
@@ -46,4 +64,15 @@ void loop() {
 
 void setChannel(int channel) {
   PORTB = channel << 1;
+}
+
+void getMask() {
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 4; j++) {
+      MASK[i][j] = 1;
+      if (j == 2 && i >= 6 && i <= 12) MASK[i][j] = 0;
+      if (j == 3 && i >= 7 && i <= 12) MASK[i][j] = 0;
+      if (i >= 14) MASK[i][j] = 0;
+    }
+  }
 }
